7cd9265f809ca0923c829e2473a82a8bed5e41d1
Endowmissy <adeolaayo31@yahoo.com>
2021-10-25 06:07:36 +0100

    feat(home-page): Add home page route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,12 @@ const fetchNumbers = require('../controller/fetchFibonacciNumbers');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.status(200).json({
+      message: `Welcome to the Fibonacci Numbers API`,
+    })
+  })
+
 router.get('/numbers', fetchNumbers);
 
 module.exports = router;
